9767a914d97353412fc51f47a6c1034e9e5b6299
pritom2001 <120891554+pritom2001@users.noreply.github.com>
2025-09-17 16:54:31 +0600

    nil

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,7 @@ export default defineConfig({
             'resources/css/content-creation.css' ,'resources/js/content-creation.js',
             'resources/css/search-engine-optimisation.css' ,'resources/js/search-engine-optimisation.js',
             'resources/css/ecommerce-development.css' ,'resources/js/ecommerce-development.js',
-            'resources/css/web-development-services.css' ,'resources/js/web-development-services.js', 
-            'resources/js/ckeditor.js'
-     ],
+            'resources/css/web-development-services.css' ,'resources/js/web-development-services.js',  ],
             refresh: true,
         }),
     ],
